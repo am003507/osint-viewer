fe1278a51c81b88e1191e3c7d2aadb79c3975ac7
bmlee <am003507>
2020-08-19 15:29:16 +0900

    minor - react material

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import TestComponent from '@components/TestComponent';
+import { Button, TextField } from '@material-ui/core';
 
 const App: React.FC = () => {
   return (
     <div>
       <TestComponent />
       Init
+      <TextField />
+      <Button>ddd</Button>
     </div>
   );
 };
